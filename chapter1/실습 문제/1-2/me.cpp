28f1a6845b4588957141097eeef865c2d7dea7ee
se0nw00 <younseonwoo17@gmail.com>
2024-10-08 21:30:19 +0900

    chapter1 깃허브에 올리기

---
+++ chapter1/실습 문제/1-2/me.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+	std::cout << "가상현실융합학과\n";
+	std::cout << "만 19세\n";
+	std::cout << "A&R\n";
+	return 0;
+}
---
+++ chapter1/실습 문제/1-4/star.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j <= i; j++) {
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
